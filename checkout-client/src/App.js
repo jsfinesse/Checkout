e82d73da143a90975376112ab5f7a6bbb52152ea
Prithu Singh <prithusingh0327@gmail.com>
2022-02-10 09:33:49 +0530

    Initial project setup commit

---
+++ checkout-client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "antd/dist/antd.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Items from "./pages/Items";
+
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/home" element={<Homepage />} />
+                    <Route path="/items" element={<Items />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
