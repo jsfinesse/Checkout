8f14fb25b157225c8fcce5799c0cfd27e7432edd
Prithu Singh <prithusingh0327@gmail.com>
2022-02-13 14:02:21 +0530

    Created antd grid to display items and made it responsive

--- checkout-client/src/components/Item.js
+++ checkout-client/src/components/Item.js
@@ -1,11 +1,15 @@
 import React from "react";
+import {Button} from "antd";
 
 function Item({ item }) {
     return (
         <div className="item">
-            <h4>{item.name}</h4>
+            <h4 className="name">{item.name}</h4>
             <img src={item.image} alt={item.name} height="100" width="100" />
-            <h4><b>Price:</b> {item.price}</h4>
+            <h4 className="price"><b>Price:</b> {item.price}</h4>
+            <div className="d-flex justify-content-end">
+                <Button>Add to Cart</Button>
+            </div>
         </div>
     );
 }
--- checkout-client/src/pages/Homepage.js
+++ checkout-client/src/pages/Homepage.js
@@ -3,6 +3,7 @@ import DefaultLayout from "../components/DefaultLayout";
 import axios from "axios";
 import {Row, Col} from "antd";
 import Item from "../components/Item";
+import "../resources/item.css";
 
 function Homepage() {
     const [itemsData, setItemsData] = useState([]);
@@ -22,9 +23,9 @@ function Homepage() {
 
     return (
         <DefaultLayout>
-            <Row>
+            <Row gutter={20}>
                 {itemsData.map((item) => {
-                    return <Col span={6}>
+                    return <Col xs={24} lg={6} md={12} sm={6}>
                         <Item item={item} />
                     </Col>
                 })}
