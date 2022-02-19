4b9b9f80708b0acbb110ccd89dbb879c19eaee70
Prithu Singh <prithusingh0327@gmail.com>
2022-02-20 00:52:25 +0530

    Added category selection in homepage

--- checkout-client/src/pages/Homepage.js
+++ checkout-client/src/pages/Homepage.js
@@ -5,10 +5,23 @@ import { useDispatch } from "react-redux";
 
 import DefaultLayout from "../components/DefaultLayout";
 import Item from "../components/Item";
-import "../resources/item.css"
+import "../resources/item.css";
 
 function Homepage() {
     const [itemsData, setItemsData] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("fruits");
+    const categories = [
+        {
+            name: "fruits",
+            imageURL:
+                "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2021%2F11%2F03%2F61129_Prep_Smoothies_030_preview-2000.jpg&q=60",
+        },
+        {
+            name: "vegetables",
+            imageURL:
+                "https://cdn.mos.cms.futurecdn.net/XM8scaSf7gWsiN9jjowikf.jpg",
+        },
+    ];
     const dispatch = useDispatch();
 
     const getAllItems = async () => {
@@ -29,14 +42,37 @@ function Homepage() {
 
     return (
         <DefaultLayout>
-            <Row gutter={20}>
-                {itemsData.map((item) => {
+            <div className="d-flex">
+                {categories.map((category) => {
                     return (
-                        <Col xs={24} lg={6} md={12} sm={6}>
-                            <Item item={item} />
-                        </Col>
+                        <div
+                            onClick={() => setSelectedCategory(category.name)}
+                            className={`d-flex category ${
+                                selectedCategory === category.name &&
+                                "selected-category"
+                            }`}
+                        >
+                            <h4>{category.name}</h4>
+                            <img
+                                src={category.imageURL}
+                                alt="category icon"
+                                height="60"
+                                width="80"
+                            />
+                        </div>
                     );
                 })}
+            </div>
+            <Row gutter={20}>
+                {itemsData
+                    .filter((i) => i.category === selectedCategory)
+                    .map((item) => {
+                        return (
+                            <Col xs={24} lg={6} md={12} sm={6}>
+                                <Item item={item} />
+                            </Col>
+                        );
+                    })}
             </Row>
         </DefaultLayout>
     );
