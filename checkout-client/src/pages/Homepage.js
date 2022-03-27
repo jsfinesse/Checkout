5cbc984bcaad53c398b3589af28c71c91ec991f3
Prithu Singh <prithusingh0327@gmail.com>
2022-03-27 13:27:28 +0530

    Connect api to client using axios

--- checkout-client/src/pages/Homepage.js
+++ checkout-client/src/pages/Homepage.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DefaultLayout from "../components/DefaultLayout";
+import axios from "axios";
 
 function Homepage() {
+    const getAllItems = async () => {
+        try {
+            const res = await axios.get("/api/items/get-all-items");
+            console.log(res.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getAllItems();
+    }, []);
+
     return (
         <DefaultLayout>
             <div>Homepage</div>
