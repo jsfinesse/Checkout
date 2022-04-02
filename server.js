3449f33f14567172ccbdad287e5514d75f7bc121
Prithu Singh <prithusingh0327@gmail.com>
2022-04-02 18:44:11 +0530

    Configured for deployment

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 app.use(express.json());
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Require DB connections and routes
 require("./dbConnection");
 const itemsRoute = require("./routes/itemsRoute");
 const usersRoute = require("./routes/usersRoute");
-const billsRoute = require("./routes/billsRoute")
+const billsRoute = require("./routes/billsRoute");
 
 app.use("/api/items/", itemsRoute);
 app.use("/api/users/", usersRoute);
 app.use("/api/bills/", billsRoute);
+
+if (process.env.NODE_ENV === "production") {
+    app.use("/", express.static("checkout-client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "checkout-client/build/index.html")
+        );
+    });
+}
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
